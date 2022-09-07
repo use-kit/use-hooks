df05023b60710eb4561a7538a4ec9b2f131bf7e8
Akashi <akashi_sai@163.com>
2022-09-07 21:36:16 +0800

    :art: feat: change structure

--- packages/core/use-age/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from 'vitest'
-import { useAge } from '.'
-
-describe('should', () => {
-  it('exported', () => {
-    expect(useAge('1997-09-28')).toMatchInlineSnapshot('24')
-  })
-})
--- packages/core/use-age/index.ts
+++
@@ -1,5 +0,0 @@
-import dayjs from 'dayjs'
-
-export const useAge = (str: string) => {
-  return dayjs().diff(str, 'year')
-}
